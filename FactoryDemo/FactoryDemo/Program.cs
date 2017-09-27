05f145b6711e681ea8e70a21152ab3d84d4546d4
Bas <basnetten@gmail.com>
2017-09-27 09:01:42 +0200

    Initialized project

---
+++ FactoryDemo/FactoryDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryDemo
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
